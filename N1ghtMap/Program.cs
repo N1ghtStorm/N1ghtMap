8c55194c1c67bccf7c6541db52bbdcc63e56ea01
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2021-01-13 20:46:02 +0300

    change to csharp

---
+++ N1ghtMap/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace N1ghtMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public class HashMap<T, E>
+    {
+        private List<T> m_array;
+        public HashMap()
+        {
+            m_array = new List<T>();
+        }
+
+        public HashMap(int size)
+        {
+            m_array = new List<T>(size);
+        }
+
+        
+    }
+}
